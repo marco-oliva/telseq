8d6e62304d7678744b02b85a3eef1d5e8f4506c0
Johnny Bravo <hakmonkey@proton.me>
2023-10-16 01:42:35 -0400

    Update deduplicate.py

--- workflow/scripts/deduplicate.py
+++ workflow/scripts/deduplicate.py
@@ -1,106 +1,49 @@
-# Copyright (c) Boucher Lab. All rights reserved.
-# Licensed under the GNU license.
-# See LICENSE file in the repository root for full license information.
+#!/usr/bin/env python3
 
-from common import *
+import argparse
+import gzip
 
-from Bio import SeqIO
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--reads', required=True)
+    parser.add_argument('--duplicates', required=True)
+    parser.add_argument('--out_reads', required=True)
+    parser.add_argument('--out_dupes', required=True)
+    return parser.parse_args()
+
+def get_duplicates(dupes_file):
+    return [line.strip() for line in open(dupes_file)]
+
+def get_reads(reads):
+    return (line for line in gzip.open(reads, 'rb'))
+
+def make_out_file(fq):
+    open(fq, 'wb').close()
+
+def write_out(outfile, read_id, seq, desc, qual):
+    with gzip.open(outfile , "a") as o:
+        o.write(read_id)
+        o.write(seq)
+        o.write(desc)
+        o.write(qual)
+
+def dup_check(read, reads, duplicates, out_reads, out_dupes):
+    read_id = read.split(b' ')[0][1:].decode('utf-8')
+    if read_id in duplicates:
+        write_out(out_dupes, read, next(reads), next(reads), next(reads))
+    else:
+        write_out(out_reads, read, next(reads), next(reads), next(reads))
+
+def gen_deduped_reads(reads, duplicates, out_reads, out_dupes):
+    [dup_check(read, reads, duplicates, out_reads, out_dupes) for read in reads]
 
 def main():
-    parser = argparse.ArgumentParser(description='Cluster reads based on read length')
-    parser.add_argument('-r', help='Reads File (FASTA/FASTQ)', type=str, dest='reads_file', required=True)
-    parser.add_argument('-d', help='Csv with duplicates sets', type=str, dest='duplicates_file', required=True)
-    args = parser.parse_args()
-
-    root_logger = init_logger()
-
-    fastq_with_dups = args.reads_file
-    sets_csv = args.duplicates_file
-
-    sets = []
-    with open(sets_csv, 'r') as sets_handle:
-        sets_csv_reader = csv.reader(sets_handle)
-        for row in sets_csv_reader:
-            curr_set_elements = []
-            for read_id in row:
-                curr_set_elements.append(read_id)
-            sets.append(set(curr_set_elements))
-
-    # Sets are not disjoint :(
-    are_sets_disjoint = False
-    itr = 0
-    while (not are_sets_disjoint):
-        # print("iteration: " + str(itr))
-        # print(len(sets))
-        itr += 1
-        are_sets_disjoint = True
-        combined_sets = []
-        used_set_indices = {}
-        for i in range(0, len(sets)):
-            used_set_indices[i] = False
-
-        for i in range(0, len(sets)):
-            if not used_set_indices[i]:
-                combined_set = sets[i]
-                used_set_indices[i] = True
-            else:
-                # print("continue to next from " + str(i))
-                continue
-
-            for j in range(i + 1, len(sets)):
-                if not used_set_indices[j]:
-                    if not len(combined_set & sets[j]) == 0:
-                        # print("merging " + str(i) + ", " + str(j))
-                        combined_set = combined_set | sets[j]
-                        are_sets_disjoint = False
-                        used_set_indices[j] = True
-                else:
-                    continue
-
-            # print("appending: " + str(i))
-            combined_sets.append(combined_set)
-
-        # print(len(combined_sets))
-        sets = combined_sets
-
-    set_deduplicated = {}
-    frozen_sets = []
-    # print(len(sets))
-    for curr_set in sets:
-        frozen_set = frozenset(curr_set)
-        frozen_sets.append(frozen_set)
-        set_deduplicated[frozen_set] = False
-
-    if (is_gz_file(fastq_with_dups)):
-        root_logger.info('Opening gzipped file')
-        file_handler = gzip.open(fastq_with_dups, 'rt')
-    else:
-        root_logger.info('Opening uncompressed file')
-        file_handler = open(fastq_with_dups, 'rt')
-
-    dedup_records = []
-    set_sizes = []
-    num_singletons = 0
-    for record in SeqIO.parse(file_handler, "fastq"):
-        record_in_set = False
-        for dup_set in frozen_sets:
-            if record.id in dup_set:
-                record_in_set = True
-                curr_record_set = dup_set
-                break
-
-        if record_in_set:
-            if not set_deduplicated[curr_record_set]:
-                dedup_records.append(record)
-                set_sizes.append(len(curr_record_set))
-                set_deduplicated[curr_record_set] = True
-        else:  # Singletons are not in the set csv
-            num_singletons += 1
-            dedup_records.append(record)
-
-    file_handler.close()
-    out_handle = sys.stdout
-    SeqIO.write(dedup_records, out_handle, "fastq")
+    args = parse_args()
+    duplicates = get_duplicates(args.duplicates)
+    reads = get_reads(args.reads)
+    make_out_file(args.out_reads)
+    make_out_file(args.out_dupes)
+    gen_deduped_reads(reads, duplicates, args.out_reads, args.out_dupes)
 
 if __name__ == "__main__":
     main()

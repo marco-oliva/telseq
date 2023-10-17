1604f73d4a491e3c624670cf2ef86be2322064c4
Johnny Bravo <hakmonkey@proton.me>
2023-10-17 05:54:55 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -31,4 +31,4 @@ module load snakemake
 snakemake --cluster "sbatch -A {cluster.account} -q {cluster.qos} -c {cluster.cpus-per-task} -N {cluster.Nodes} \
   -t {cluster.runtime} --mem {cluster.mem} -J {cluster.jobname} --mail-type={cluster.mail_type} \
   --mail-user={cluster.mail} --output {cluster.out} --error {cluster.err}" \
-  --cluster-config config/cluster.json --jobs 100 --latency-wait 20 --rerun-incomplete --use-envmodules
+  --cluster-config config/cluster.json --jobs 100 --latency-wait 20 --rerun-incomplete --use-conda
